5c7fa316101b4db9dd58cc46346c48741dfe2b36
sagyobake <sagyobake@localhost.localdomain>
2025-02-06 20:11:39 +0900

    upgrade

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
-const factorials_list = document.getElementById("factorials_list");
+const result_list = document.getElementById("list_result");
+const result_graph = document.getElementById("graph_result");
 
 //For文で階乗を求める
 const calcByForLoop = (n) => {
@@ -20,24 +21,24 @@ const stirlingApproximation = (n) => {
 
 //計算結果をDOMに渡す
 const passToDom = (n, a, b) => {
-    console.log(n, a, b);
+    //console.log(n, a, b);
     const div = document.createElement("div");
     const a_p = document.createElement("p");
     const b_p = document.createElement("p");
     const d_p = document.createElement("p");
-    const d = Math.abs(b - a) / a * 100;
+    const d = 100 - Math.abs(b - a) / a * 100;
     div.setAttribute("id", "result_div");
     a_p.innerText = `${n}! = ${a}`;
     b_p.innerText = `${n}! = ${b}`;
-    d_p.innerText = `誤差　${d.toFixed(5)}%`;
+    d_p.innerText = `近似率　${d.toFixed(5)}%`;
     div.appendChild(a_p);
     div.appendChild(b_p);
     div.appendChild(d_p);
-    factorials_list.appendChild(div);
+    result_list.appendChild(div);
 };
 
 //For文を用いて階乗の一覧を表示する
-for (let i = 1; i <= 200; i++) {
+for (let i = 1; i <= 170; i++) {
     const a = calcByForLoop(i);
     const b = stirlingApproximation(i);
     passToDom(i, a, b);
